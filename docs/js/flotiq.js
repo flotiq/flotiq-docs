a3da4ec4e2af0762a91be9d44b31152a901d700c
KInga Knapik <knapik.kinga@gmail.com>
2021-10-29 11:49:54 +0200

    new visual identification for Flotiq, fixes

--- docs/js/flotiq.js
+++ docs/js/flotiq.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     load_navpane();
 });
 
@@ -9,7 +9,7 @@ function load_navpane() {
     }
 
     var nav = document.getElementsByClassName("md-nav");
-    for(var i = 0; i < nav.length; i++) {
+    for (var i = 0; i < nav.length; i++) {
         if (typeof nav.item(i).style === "undefined") {
             continue;
         }
@@ -21,31 +21,12 @@ function load_navpane() {
     }
 
     var nav = document.getElementsByClassName("md-nav__toggle");
-    for(var i = 0; i < nav.length; i++) {
-       nav.item(i).checked = true;
+    for (var i = 0; i < nav.length; i++) {
+        nav.item(i).checked = true;
     }
 }
-window.addEventListener('click', function (event) {
-    setTimeout(() => {
-        if (event.target.className === 'md-nav__link') {
-            document.querySelectorAll('pre code').forEach((block) => {
-                hljs.highlightBlock(block);
-            });
-        }
-    }, 200);
-    //fallback if page do not rendered in 200ms
-    setTimeout(() => {
-        if (event.target.className === 'md-nav__link') {
-            document.querySelectorAll('pre code').forEach((block) => {
-                hljs.highlightBlock(block);
-            });
-        }
-    }, 200);
-});
 
-hljs.initHighlightingOnLoad();
-
-document.addEventListener("DOMContentLoaded", function(event) {
+function addHelpfulForm() {
     if (document.querySelector('#was-it-helpful')) {
         new WasItHelpful('#was-it-helpful', {
             labels: {
@@ -56,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 "submit_btn": "Send feedback",
                 "thank_you": "Thanks!"
             },
-            onSubmit: function(data) {
+            onSubmit: function (data) {
                 var xhr = new XMLHttpRequest();
                 xhr.open("POST", 'https://api.flotiq.com/api/v1/content/doc_helpful_articles_info', true);
                 xhr.setRequestHeader('Content-Type', 'application/json');
@@ -67,6 +48,89 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     'helpful': data.helpful,
                     'message': data.message
                 }));
-            }});
+            }
+        });
     }
+}
+
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(() => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+function isNavigationLink(event) {
+    if (event.target.className === 'md-nav__link') {
+        return true;
+    }
+    if (event.target.className === 'md-footer__direction') {
+        return true;
+    }
+    if(event.target.className === 'md-flex md-footer__link md-footer__link--next') {
+        return true;
+    }
+    if(event.target.className === 'md-flex md-footer__link md-footer__link--prev') {
+        return true;
+    }
+    if(event.target.className === 'md-flex__cell md-flex__cell--stretch md-footer__title') {
+        return true;
+    }
+    if(event.target.className === 'md-flex__ellipsis') {
+        return true;
+    }
+    return false;
+}
+
+function updateScripts(event) {
+    setTimeout(() => {
+        if (isNavigationLink(event)) {
+            document.querySelectorAll('pre code').forEach((block) => {
+                hljs.highlightBlock(block);
+            });
+        }
+    }, 200);
+}
+
+window.addEventListener('click', function (event) {
+    updateScripts(event);
+    setTimeout(() => {
+        if (isNavigationLink(event)) {
+            waitForElm('#was-it-helpful').then(() => addHelpfulForm());
+            waitForElm('#disqus_thread').then(() => {
+                DISQUS.reset({
+                    reload: true,
+                    config: function () {
+                        this.page.identifier = window.location.pathname.substr(1);
+                        this.page.url = window.location.href;
+                        this.page.title = document.title;
+                        this.language = 'en';
+                    }
+                });
+            });
+        }
+    }, 300);
+    //fallback if page do not rendered in 200ms
+    updateScripts(event);
 });
+
+hljs.initHighlightingOnLoad();
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    addHelpfulForm();
+});
+
+addHelpfulForm();
